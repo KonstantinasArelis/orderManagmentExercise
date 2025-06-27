b41b305f7ddc1de032e6e74b23c00d056371d92f
Konstantinas Arelis <arelis.konstantinas@gmail.com>
2025-06-27 14:50:14 +0300

    Move repository interfaces to data access

---
+++ OrderManagment.DataAccess/Interfaces/IProductRepository.cs
@@ -0,0 +1,10 @@
+using OrderManagment.DataAccess.Entities;
+
+namespace OrderManagment.BusinessLogic.Interfaces;
+
+public interface IProductRepository
+{
+    public int SaveProduct(ProductEntity product);
+    public IReadOnlyCollection<ProductEntity> RetrieveProducts(String productName);
+    public IReadOnlyCollection<ProductEntity> RetrieveDiscountedProducts();
+}
